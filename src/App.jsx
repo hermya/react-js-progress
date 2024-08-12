54c151dab85cabc8fc33d3149e8d8f49c6006dc1
Heramb Joshi <herambku@gmail.com>
2024-08-12 15:39:14 -0500

    Added example to demonstrate importing a non-js file (like css/assets) in javascript to be used directly in html attributes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import reactImg from './assets/react-core-concepts.png';
 const reactDescriptions = ['Fundamental','Crucial','Important', 'Core']
 
 function Header() {
@@ -6,7 +7,7 @@ function Header() {
   }
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {reactDescriptions[getRandomInt()]} React concepts you will need for almost any app you are
