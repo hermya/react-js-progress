31840223467313c7b101071a45193859597fdd16
Heramb Joshi <herambku@gmail.com>
2024-08-14 13:59:13 -0500

    Added code to demonstrate how to extract jsx code to a variable and use them in jsx 'slots' to be sent inside a component

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,28 +1,31 @@
 import { useState } from "react";
 import { EXAMPLES } from "../data";
 import TabButton from "./TabButton";
+import Tabs from "./Tabs";
 
 export default function Examples() {
     const [tabContent, setTabContent] = useState();
     const [selectedTab, setSelectedTab] = useState();
-  
+
     const handleClick = (selectedButton) => {
-      setTabContent(EXAMPLES[selectedButton]);
-      setSelectedTab(selectedButton);
+        setTabContent(EXAMPLES[selectedButton]);
+        setSelectedTab(selectedButton);
     };
+
+    const tabButtons = <>
+        <TabButton onClick={() => handleClick('components')} isSelected={selectedTab == 'components'} >Components</TabButton>
+        <TabButton onClick={() => handleClick('jsx')} isSelected={selectedTab == 'jsx'}>JSX</TabButton>
+        <TabButton onClick={() => handleClick('props')} isSelected={selectedTab == 'props'}>Props</TabButton>
+        <TabButton onClick={() => handleClick('state')} isSelected={selectedTab == 'state'}>States</TabButton>
+    </>;
     return (
         <section id="examples">
             <h2>
                 Examples
             </h2>
-            <menu>
-                <TabButton onClick={() => handleClick('components')} isSelected={selectedTab == 'components'} >Components</TabButton>
-                <TabButton onClick={() => handleClick('jsx')} isSelected={selectedTab == 'jsx'}>JSX</TabButton>
-                <TabButton onClick={() => handleClick('props')} isSelected={selectedTab == 'props'}>Props</TabButton>
-                <TabButton onClick={() => handleClick('state')} isSelected={selectedTab == 'state'}>States</TabButton>
-            </menu>
-            {
-                !tabContent ?
+            <Tabs buttons={tabButtons}>
+                {
+                    !tabContent ?
                     <p>Please select a topic!</p> :
                     <div id="tab-content">
                         <h3>{tabContent.title}</h3>
@@ -31,7 +34,7 @@ export default function Examples() {
                             <code>{tabContent.code}</code>
                         </pre>
                     </div>
-            }
-
+                }
+            </Tabs>
         </section>);
 }
